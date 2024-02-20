5aeb2c466ace181d794f816265debf3e1f0b8e0f
Tito <61942255+TitoManri@users.noreply.github.com>
2024-02-20 15:14:08 -0600

    Semana 6 parte 1
    No me dejo con la carpeta target decia "no mas de 100 MB"

--- src/main/java/com/tienda_l/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda_l/service/impl/CategoriaServiceImpl.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda_l.service.impl;
 
 import com.tienda_l.dao.CategoriaDao;
@@ -10,22 +6,39 @@ import com.tienda_l.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-/**
- *
- * @author manri
- */
 @Service
-public class CategoriaServiceImpl 
-        implements CategoriaService{
-    
-    
+public class CategoriaServiceImpl implements CategoriaService {
+
     @Autowired
     private CategoriaDao categoriaDao;
-    
+
     @Override
-    public List<Categoria> getCategorias(boolean activos){
-    return categoriaDao.findAll();
-}
-    
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(c -> !c.isActivo());
+        }
+        return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
 }
